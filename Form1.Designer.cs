da70910d1f7f083cc5bc3da6c42cc5c76e8b4508
Koocy <67228486+Koocy@users.noreply.github.com>
2022-05-16 17:56:45 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Space_Invaders
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lilguy = new System.Windows.Forms.Button();
+            this.moveLilGuy = new System.Windows.Forms.Timer(this.components);
+            this.moveBullet = new System.Windows.Forms.Timer(this.components);
+            this.moveEnemies = new System.Windows.Forms.Timer(this.components);
+            this.moveBullet2 = new System.Windows.Forms.Timer(this.components);
+            this.GameOverLine = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lilguy
+            // 
+            this.lilguy.BackColor = System.Drawing.Color.Green;
+            this.lilguy.FlatAppearance.BorderSize = 0;
+            this.lilguy.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lilguy.Location = new System.Drawing.Point(225, 425);
+            this.lilguy.Name = "lilguy";
+            this.lilguy.Size = new System.Drawing.Size(50, 20);
+            this.lilguy.TabIndex = 0;
+            this.lilguy.UseVisualStyleBackColor = false;
+            this.lilguy.Click += new System.EventHandler(this.lilguy_Click);
+            this.lilguy.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.lilguy_previewKeyDown);
+            // 
+            // moveLilGuy
+            // 
+            this.moveLilGuy.Tick += new System.EventHandler(this.moveLilGuy_Tick);
+            // 
+            // moveBullet
+            // 
+            this.moveBullet.Interval = 25;
+            this.moveBullet.Tick += new System.EventHandler(this.moveBullet_Tick);
+            // 
+            // moveEnemies
+            // 
+            this.moveEnemies.Interval = 750;
+            this.moveEnemies.Tick += new System.EventHandler(this.moveEnemies_Tick);
+            // 
+            // moveBullet2
+            // 
+            this.moveBullet2.Interval = 25;
+            this.moveBullet2.Tick += new System.EventHandler(this.moveBullet2_Tick);
+            // 
+            // GameOverLine
+            // 
+            this.GameOverLine.Enabled = false;
+            this.GameOverLine.FlatAppearance.BorderSize = 0;
+            this.GameOverLine.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.GameOverLine.Location = new System.Drawing.Point(0, 410);
+            this.GameOverLine.Name = "GameOverLine";
+            this.GameOverLine.Size = new System.Drawing.Size(510, 10);
+            this.GameOverLine.TabIndex = 1;
+            this.GameOverLine.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(504, 459);
+            this.Controls.Add(this.GameOverLine);
+            this.Controls.Add(this.lilguy);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "score: 0";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button lilguy;
+        private System.Windows.Forms.Timer moveLilGuy;
+        private System.Windows.Forms.Timer moveBullet;
+        private System.Windows.Forms.Timer moveEnemies;
+        private System.Windows.Forms.Timer moveBullet2;
+        private System.Windows.Forms.Button GameOverLine;
+
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,272 @@
+using System;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Space_Invaders
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Button bullet = new Button();
+        Button bullet2 = new Button();
+
+        static int numberOfEnemies = 6, numberOfKills = 0, level = 0, score = 0, counter = 0, speed = 5;
+
+        bool left_IsPressed = false, right_IsPressed = false, BulletisRemoved = true, BulletisRemoved2 = true;
+
+        public void Form1_Load(object sender, EventArgs e)
+        {
+            moveLilGuy.Start();
+            moveEnemies.Start();
+            moveBullet2.Start();
+
+            bullet.Size = new Size(10, 10);
+            bullet.BackColor = Color.Black;
+
+            bullet2.FlatStyle = FlatStyle.Flat;
+            bullet2.BackColor = Color.Red;
+            bullet2.Size = new Size(10, 10);
+
+            createEnemies();
+        }
+
+        public void createEnemies()
+        {
+            int left = -50;
+            int top = 20;
+
+            int x = 1;
+            for (int i = 1; i <= numberOfEnemies; i++)
+            {
+                Label enemy = new Label();
+                enemy.Size = new Size(40, 15);
+                enemy.BackColor = Color.Black;
+
+                    if (i == x*(numberOfEnemies / (level + 1)) + 1)
+                    {
+                        top = top + 40;
+                        left = 30;
+                        x++;
+                    }
+
+                    else
+                    {
+                        left = left + 80;
+                    }
+
+                enemy.Top = top;
+                enemy.Left = left;
+
+                Controls.Add(enemy);
+                enemy.BringToFront();
+            }
+        }
+
+        private void lilguy_previewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            e.IsInputKey = true;
+
+            switch (e.KeyCode)
+            {
+                case Keys.A:
+                case Keys.Left:
+
+                    if (lilguy.Left > 20)
+                    {
+                        lilguy.Left = lilguy.Left - 10;
+
+                        left_IsPressed = true;
+                        right_IsPressed = false;
+                    }
+
+                    break;
+
+                case Keys.D:
+                case Keys.Right:
+
+                    if (lilguy.Right < 480)
+                    {
+                        lilguy.Left = lilguy.Left + 10;
+
+                        right_IsPressed = true;
+                        left_IsPressed = false;
+                    }
+
+                    break;
+            }
+        }
+
+
+
+        public void lilguy_Click(object sender, EventArgs e)
+        {
+
+            if (BulletisRemoved)
+            {
+                bullet.Top = lilguy.Top - lilguy.Size.Height;
+                bullet.Left = ((lilguy.Left + lilguy.Right) / 2) - 5;
+
+                Controls.Add(bullet);
+                bullet.BringToFront();
+
+                moveBullet.Start();
+                BulletisRemoved = false;
+            }
+
+        }
+
+        private void moveLilGuy_Tick(object sender, EventArgs e)
+        {
+            if (left_IsPressed)
+            {
+                lilguy.Left = lilguy.Left - 5;
+            }
+
+            if (right_IsPressed)
+            {
+                lilguy.Left = lilguy.Left + 5;
+            }
+
+            if (lilguy.Left < 20)
+            {
+                right_IsPressed = true;
+                left_IsPressed = false;
+            }
+
+            if (lilguy.Right > 480)
+            {
+                left_IsPressed = true;
+                right_IsPressed = false;
+            }
+        }
+
+        private void moveBullet_Tick(object sender, EventArgs e)
+        {
+            bullet.Top = bullet.Top - 10;
+
+            if (bullet.Top <= 0)
+            {
+                Controls.Remove(bullet);
+                BulletisRemoved = true;
+                moveBullet.Stop();
+            }
+
+            foreach (Label labels in Controls.OfType<Label>())
+            {
+                if (bullet.Bounds.IntersectsWith(labels.Bounds))
+                {
+                    moveBullet.Stop();
+
+                    Controls.Remove(bullet);
+                    Controls.Remove(labels);
+                    BulletisRemoved = true;
+
+                    score = score + 100;
+                    numberOfKills++;
+                    Text = "score: " + score;
+                }
+
+            }
+
+            if (numberOfKills == numberOfEnemies)
+            {
+                numberOfEnemies = numberOfEnemies + 6;
+                numberOfKills = 0;
+                level++;
+                moveEnemies.Interval = moveEnemies.Interval - 50;
+                speed = speed + 1;
+
+                createEnemies();
+            }
+
+        }
+
+        private void moveEnemies_Tick(object sender, EventArgs e)
+        {
+
+            if (counter % 2 != 0)
+            {
+                foreach (Label labels in Controls.OfType<Label>())
+                {
+                    labels.Left = labels.Left + 10;
+                }
+
+                counter = 0;
+            }
+
+            else if (counter % 2 == 0)
+            {
+                foreach (Label labels in Controls.OfType<Label>())
+                {
+                    labels.Left = labels.Left - 10;
+                }
+
+                counter++;
+            }
+
+            foreach (Label labels in Controls.OfType<Label>())
+            {
+                labels.Top = labels.Top + 10;
+
+                if (labels.Bounds.IntersectsWith(GameOverLine.Bounds))
+                {
+                    moveLilGuy.Stop();
+                    moveBullet.Stop();
+                    moveEnemies.Stop();
+                    moveBullet2.Stop();
+                    
+                    lilguy.Enabled = false;
+
+                    MessageBox.Show("Game Over. Enemies reached you.");
+
+                    break;
+                }
+
+            }
+
+        }
+
+        private void moveBullet2_Tick(object sender, EventArgs e)
+        {
+            if (BulletisRemoved2)
+            {
+                Random random = new Random();
+                int random_int = random.Next(1, 7);
+
+                bullet2.Top = 20;
+                bullet2.Left = 30 * random_int;
+
+                Controls.Add(bullet2);
+                bullet2.BringToFront();
+
+                BulletisRemoved2 = false;
+            }
+
+            if (bullet2.Bounds.IntersectsWith(lilguy.Bounds))
+            {
+                moveLilGuy.Stop();
+                moveBullet.Stop();
+                moveEnemies.Stop();
+                moveBullet2.Stop();
+                
+                lilguy.Enabled = false;
+
+                MessageBox.Show("Game over. You were shot.");
+            }
+
+            if (bullet2.Bottom >= 500)
+            {
+                Controls.Remove(bullet2);
+                BulletisRemoved2 = true;
+            }
+
+            bullet2.Top = bullet2.Top + speed;
+        }
+
+    }
+}
